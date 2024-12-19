dc276f605a73c7dec14e807e7e312021da3e0337
RosieTheGhostie <mdcartkids@gmail.com>
2024-12-18 21:02:56 -0700

    added `set_color!` and `reset_color!` made file context opt-in

--- minimal-logging-macros/src/lib.rs
+++ minimal-logging-macros/src/lib.rs
@@ -1,5 +1,40 @@
 #[macro_export]
+macro_rules! set_color {
+    (debug) => {
+        eprint!("\x1B[35m");
+    };
+    (warn) => {
+        eprint!("\x1B[33m");
+    };
+    (error) => {
+        eprint!("\x1B[31m");
+    };
+    (fatal) => {
+        eprint!("\x1B[91m");
+    };
+}
+
+#[macro_export]
+macro_rules! reset_color {
+    () => {
+        eprint!("\x1B[0m");
+    };
+}
+
+#[macro_export]
 macro_rules! debugln {
+    () => {
+        eprintln!()
+    };
+    ($($arg:tt)*) => {{
+        eprint!("\x1B[35m[DEBUG] ");
+        eprintln!($($arg)*);
+        eprint!("\x1B[0m");
+    }};
+}
+
+#[macro_export]
+macro_rules! debugln_context {
     () => {
         eprintln!()
     };
@@ -16,6 +51,18 @@ macro_rules! warnln {
         eprintln!()
     };
     ($($arg:tt)*) => {{
+        eprint!("\x1B[33m[WARNING] ");
+        eprintln!($($arg)*);
+        eprint!("\x1B[0m");
+    }};
+}
+
+#[macro_export]
+macro_rules! warnln_context {
+    () => {
+        eprintln!()
+    };
+    ($($arg:tt)*) => {{
         eprint!("\x1B[33m[WARNING ({}:{}:{})] ", file!(), line!(), column!());
         eprintln!($($arg)*);
         eprint!("\x1B[0m");
@@ -28,6 +75,18 @@ macro_rules! errorln {
         eprintln!()
     };
     ($($arg:tt)*) => {{
+        eprint!("\x1B[31m[ERROR] ");
+        eprintln!($($arg)*);
+        eprint!("\x1B[0m");
+    }};
+}
+
+#[macro_export]
+macro_rules! errorln_context {
+    () => {
+        eprintln!()
+    };
+    ($($arg:tt)*) => {{
         eprint!("\x1B[31m[ERROR ({}:{}:{})] ", file!(), line!(), column!());
         eprintln!($($arg)*);
         eprint!("\x1B[0m");
@@ -36,6 +95,18 @@ macro_rules! errorln {
 
 #[macro_export]
 macro_rules! fatalln {
+    () => {
+        eprintln!()
+    };
+    ($($arg:tt)*) => {{
+        eprint!("\x1B[91m[FATAL] ");
+        eprintln!($($arg)*);
+        eprint!("\x1B[0m");
+    }};
+}
+
+#[macro_export]
+macro_rules! fatalln_context {
     () => {
         eprintln!()
     };
